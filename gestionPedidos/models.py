eddd77dd40196bc7312d22c1815871b545ccc275
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-09-22 17:57:09 -0300

    Proyecto 4

---
+++ gestionPedidos/models.py
@@ -0,0 +1,22 @@
+from statistics import mode
+from django.db import models
+
+# Create your models here.
+
+#Crear tablas Clientes, Articulos, Pedidos
+
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    email=models.EmailField()
+    telefono=models.CharField(max_length=10)
+
+class Articulos(models.Model):
+    nombre = models.CharField(max_length=30)
+    categoria = models.CharField(max_length=20)
+    precio = models.IntegerField()
+
+class Pedidos(models.Model):
+    nrodcto = models.IntegerField()
+    fecha_emision = models.DateField()
+    entregado = models.BooleanField()
---
+++ gestionPedidos/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+#python -m django startproject proyecto4  #crear proyecto
+#cd proyecto4 # abrir carpeta
+#python manage.py startapp gestionPedidos # crear APP
